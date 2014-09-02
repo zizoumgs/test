c6b1ec8710b1b2f4e90a5e678e97047cc62a244d
Sakip Alias ZizouMgs <zizoumgs@hotmail.com>
2014-09-03 06:38:21 +0700

    Test my first folder git

---
+++ test_folder/cha_2_1.php
@@ -0,0 +1,30 @@
+<html>
+<head>
+	<title>2.1.1 Defining Text Strings</title>
+	</head>
+<body>
+		<h3> Following text is  from book</h3>
+	<p>
+2.1.1 Defining Text Strings
+There are a few ways to indicate a string in a PHP program. The simplest is to surround the string with single quotes:
+
+print 'I would like a bowl of soup.';
+
+print 'chicken';
+
+print '06520';
+
+print '"I am eating dinner," he growled.';
+	</p>
+	<h3>Your php should like following </h3>
+	<?php
+print 'I would like a bowl of soup.';
+
+print 'chicken';
+
+print '06520';
+
+print '"I am eating dinner," he growled.';
+?>
+</body>
+</html>
